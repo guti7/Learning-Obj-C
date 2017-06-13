07a35ed26d290b1f7c6373850c7b5691fc884693
José Gutiérrez <guti7@users.noreply.github.com>
2017-06-13 13:21:46 -0700

    Add Asset class.

---
+++ PartII/BMITime/BMITime/Asset.h
@@ -0,0 +1,16 @@
+//
+//  Asset.h
+//  BMITime
+//
+//  Created by Guti on 6/13/17.
+//  Copyright © 2017 Guti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Asset : NSObject
+
+@property (nonatomic, copy) NSString *label;
+@property (nonatomic) unsigned int resaleValue;
+
+@end
---
+++ PartII/BMITime/BMITime/Asset.m
@@ -0,0 +1,23 @@
+//
+//  Asset.m
+//  BMITime
+//
+//  Created by Guti on 6/13/17.
+//  Copyright © 2017 Guti. All rights reserved.
+//
+
+#import "Asset.h"
+
+@implementation Asset
+
+// Override `dealloc`
+- (void)dealloc {
+    NSLog(@"deallocating %@", self); // will send message [self description]
+}
+
+// Override `description`
+- (NSString *)description {
+    return [NSString stringWithFormat:@"<%@: $%u>", self.label, self.resaleValue];
+}
+
+@end
