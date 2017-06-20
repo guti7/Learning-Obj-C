f4f8db52e3ebb3be1beba50db9d25d8ac6b0e0e4
José Gutiérrez <guti7@users.noreply.github.com>
2017-06-19 20:25:55 -0700

    Explain #define versus global variables.

--- PartII/Constants/Constants/main.m
+++ PartII/Constants/Constants/main.m
@@ -12,6 +12,13 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSLog(@"\u03c0 is %f", M_PI);
         NSLog(@"%d is larger", MAX(10, 12));
+        
+        // `NSLocale`
+        NSLocale *here = [NSLocale currentLocale];
+        //NSString *currency = [here objectForKey:@"currency"];
+        NSString *currency = [here objectForKey:NSLocaleCurrencyCode];
+        NSString *currencySymbol = [here objectForKey:NSLocaleCurrencySymbol];
+        NSLog(@"Locale: %@, Money is %@, symbol: %@", here, currency, currencySymbol);
     }
     return 0;
     
@@ -21,5 +28,9 @@ int main(int argc, const char * argv[]) {
     /// `#include` vs. `#import`: They do essentially the same thing, the preprocessor reads a file and adds it to its output. You include a header (declarations) file and the compiler uses this to understand the code it is compiling. `#import` signifies the preprocessor to include the file once. `#include` will allow you to include the same file many times.
     
     /// `#define` is a preprocessor directive that tells the compiler to replace 'A' with 'B'. When it's used to do function-like stuff instead of simply substituting a value, you are creating a 'macro'.
+    
+    /// `enum` helps you define a set of constants. Use  `NS_ENUM()`.
+    
+    /// `#define` vs. global variables: In some cases, there are performance advantages to using global variables and global variables are easier to work with when you are in the debugger. In general, use global variables and `enum` for constants, instead of `#define`
     
 }
