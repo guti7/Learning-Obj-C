c57262a85516bc1f9aa5959c1cc49d25af0c7320
José Gutiérrez <guti7@users.noreply.github.com>
2017-06-21 07:55:53 -0700

    Add error handling to writing to file.

--- PartII/Stringz/Stringz/main.m
+++ PartII/Stringz/Stringz/main.m
@@ -15,8 +15,18 @@ int main(int argc, const char * argv[]) {
             [str appendString:@"Guti is ready!\n"];
         }
         
-        [str writeToFile:@"/tmp/cool.txt" atomically:YES encoding:NSUTF8StringEncoding error:NULL];
-        NSLog(@"done writing /tmp/cool.txt");
+        // Declare a pointer to an `NSError` object, but do no instantiate it.
+        // The `NSError` instance will only be created if there is, in fact, an error.
+        NSError *error;
+        
+        // Pass the `NSError` pointer by reference
+        BOOL success = [str writeToFile:@"/tmp/cool.txt" atomically:YES encoding:NSUTF8StringEncoding error:&error];
+        // Test the returned `BOOL`, and query the `NSError` if the write failed
+        if (success) {
+            NSLog(@"done writing /tmp/cool.txt");
+        } else {
+            NSLog(@"writing /tmp/cool.txt failed: %@", [error localizedDescription]);
+        }
     }
     return 0;
 }
