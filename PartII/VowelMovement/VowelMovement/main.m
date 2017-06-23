3b311db18a7482527b31b65a049992ab8ec69b2b
José Gutiérrez <guti7@users.noreply.github.com>
2017-06-23 07:30:57 -0700

    Use a block to remove vowels from individual strings in an array.

--- PartII/VowelMovement/VowelMovement/main.m
+++ PartII/VowelMovement/VowelMovement/main.m
@@ -22,6 +22,27 @@ int main(int argc, const char * argv[]) {
         
         // list of vowels
         NSArray *vowels = @[@"a", @"e", @"i", @"o", @"u"];
+        
+        /// Compose a block that makes a copy of a given string, removes the vowels from the copied string, and then adds this string to `devowelizedStrings`
+        // Declare a block variable
+        void (^devowelizer)(id, NSUInteger, BOOL *); // just like a function declaration - the type is 'a block that takes an object, an integer, and a BOOL pointer, and returns nothing.'
+        
+        // Compose block of the declared type and assign it to the new variable
+        devowelizer = ^(id string, NSUInteger i, BOOL *stop) {
+            NSMutableString *newString = [NSMutableString stringWithString:string];
+            
+            // Iterate over the array of vowels, replacing occurrances of each with an empty string
+            for (NSString *s in vowels) {
+                NSRange fullRange = NSMakeRange(0, [newString length]);
+                [newString replaceOccurrencesOfString:s withString:@"" options:NSCaseInsensitiveSearch range:fullRange];
+            }
+            
+            [devowelizedStrings addObject:newString];
+        }; // End of block assignment
+        
+        // Send `enumerateObjectUsingBlock:` message to `orirginalStrings` to iterate over the array with the `devowelizer` block
+        [originalStrings enumerateObjectsUsingBlock:devowelizer];
+        NSLog(@"Strings with no vowels: %@", devowelizedStrings);
     }
     return 0;
 }
