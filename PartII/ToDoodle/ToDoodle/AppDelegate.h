ffd96cd8f89a740530f1ca0ce3b75b76e491d4cb
José Gutiérrez <guti7@users.noreply.github.com>
2017-06-24 08:12:03 -0700

    Add main properties.

---
+++ PartII/ToDoodle/ToDoodle/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  ToDoodle
+//
+//  Created by Guti on 6/23/17.
+//  Copyright © 2017 Guti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (nonatomic) UITableView *taskTable;
+@property (nonatomic) UITextField *taskField;
+@property (nonatomic) UIButton *insertButton;
+
+@property (nonatomic) NSMutableArray *tasks;
+
+- (void)addTask:(id)sender;
+
+@end
+
---
+++ PartII/ToDoodle/ToDoodle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToDoodle
+//
+//  Created by Guti on 6/23/17.
+//  Copyright © 2017 Guti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
