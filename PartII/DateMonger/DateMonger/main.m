497374acd2eb4991e8078c74ad9fa0ff96fca39e
José Gutiérrez <guti7@users.noreply.github.com>
2017-06-30 10:40:46 -0700

    Create a date monger project.

---
+++ PartII/DateMonger/DateMonger/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DateMonger
+//
+//  Created by Guti on 6/30/17.
+//  Copyright © 2017 Guti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
