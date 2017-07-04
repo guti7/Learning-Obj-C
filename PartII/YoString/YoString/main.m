a2c37d35beb12c547d4b4bc1eea0fc9983fd89d1
José Gutiérrez <guti7@users.noreply.github.com>
2017-07-03 18:09:52 -0700

    C pointer types.

--- PartII/YoString/YoString/main.m
+++ PartII/YoString/YoString/main.m
@@ -6,15 +6,50 @@
 //  Copyright © 2017 Guti. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+// C library headers
+#include <stdio.h> // for printf
+#include <stdlib.h> // for malloc/free
+#include <string.h> // for strlen
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        char x = 0x21; // use ASCII as literals wrapped in single quotes - '!'
-        while (x <= 0x7e) { // '~'
-            printf("%d = 0x%x is %c\n", x, x, x);
-            x++;
-        }
+    char x = 0x21; // use ASCII as literals wrapped in single quotes - '!'
+    while (x <= 0x7e) { // '~'
+        printf("%d = 0x%x is %c\n", x, x, x);
+        x++;
     }
+    
+    // Get a pointer to 5 bytes of memory on the heap
+    char *start = malloc(5);
+    
+    // Put characters in the allocation
+    *start = 'L';
+    *(start + 1) = 'o'; // pointer arithmetic: next byte
+    *(start + 2) = 'v'; // shorthand is start[2], start[3], start[0], and so on.
+    *(start + 3) = 'e';
+    *(start + 4) = '\0';
+    
+    // Print it out
+    printf("%s has %zu characters\n", start, strlen(start));
+    
+    // Print out the third letter
+    printf("The third letter is %c\n", *(start + 2)); // dereference the second byte from start.
+    
+    // Free the memory so that it can be reused
+    free(start);
+    start = NULL;
+    
+    // Floating point numbers
+    float *favorites = malloc(3 * sizeof(float));
+    
+    favorites[0] = 3.14158;
+    favorites[1] = 2.71828;
+    favorites[2] = 1.41421;
+    
+    for (int i = 0; i < 3; i++) {
+        printf("%.4f is favorite %d\n", favorites[i], i);
+    }
+    
+    free(favorites);
+    favorites = NULL;
     return 0;
 }
